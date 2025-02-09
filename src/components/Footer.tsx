e8f6cb6635b5ed425486090c2cc7897a2ad69447
Muhammad Iqbal <52034320+iqbalev@users.noreply.github.com>
2025-02-09 18:55:02 +0700

    chore: migrate project to React.js with TypeScript using Vite

---
+++ src/components/Footer.tsx
@@ -0,0 +1,26 @@
+import iconGitHub from "../assets/icons/icon-github.svg";
+import iconGmail from "../assets/icons/icon-gmail.svg";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className="socials">
+        <p>Let's connect to build something, or if you want to collaborate!</p>
+
+        <div className="social-links">
+          <a href="https://github.com/iqbalev">
+            <img src={iconGitHub} alt="icon-github" />
+          </a>
+
+          <a href="mailto:muhammadiqbal.090300@gmail.com">
+            <img src={iconGmail} alt="icon-gmail" />
+          </a>
+        </div>
+      </div>
+
+      <p>&copy; Muhammad Iqbal. 2025.</p>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ src/components/Header.tsx
@@ -0,0 +1,31 @@
+const Header = () => {
+  return (
+    <header>
+      <h1>
+        LabQi.<span>dev</span>
+      </h1>
+
+      <nav>
+        <ul>
+          <li>
+            <a href="#about">About</a>
+          </li>
+
+          <li>
+            <a href="#tech">Technologies</a>
+          </li>
+
+          <li>
+            <a href="#projects">Projects</a>
+          </li>
+
+          <li>
+            <a href="#contact">Contact</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
